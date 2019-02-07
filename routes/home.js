f654eb68c16096ecddbdbcf773850e6b7c5ae334
George Hu <george98.hu@gmail.com>
2019-02-07 15:56:09 -0800

    created navigation between games and home screen

--- routes/home.js
+++ routes/home.js
@@ -3,5 +3,20 @@
  */
 
 exports.view = function(req, res){
-  res.render('home');
+  res.render('home', {
+    'games': [
+      { 'name': 'Game1',
+        'image': 'game1.png',
+        'id': 'game1'
+      },
+      { 'name': 'Game2',
+        'image': 'game2.png',
+        'id': 'game2'
+      },
+      { 'name': 'Game3',
+        'image': 'game3.png',
+        'id': 'game3'
+      }
+    ]
+  });
 };
---
+++ routes/viewgames.js
@@ -0,0 +1,8 @@
+/*
+ * GET view page.
+ */
+
+exports.view = function(req, res){
+    res.render('viewgames');
+  };
+  
