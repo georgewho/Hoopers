1b6cffde666332fdeec4d77ea54d04fdd628a9b4
George Hu <george98.hu@gmail.com>
2019-02-12 19:09:22 -0800

    Implemented JSON abilities for stats

--- routes/mystats.js
+++ routes/mystats.js
@@ -1,8 +1,10 @@
+//Get our stats data
+var data = require('../data.json');
 
 /*
  * GET mystats page.
  */
 
-exports.view = function(req, res){
-  res.render('mystats');
+exports.view = function(req, res) {
+    res.render('mystats', data);
 };
