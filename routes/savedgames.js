ae1846223892eb151c998e704ad12110d4565288
Ashley Chen <yjc029@ucsd.edu>
2019-02-21 15:19:18 -0800

    working on it..

--- routes/savedgames.js
+++ routes/savedgames.js
@@ -1,10 +1,24 @@
 //Get our game data
-var gamedata = require('../game.json');
+var gamedata = require('../game.json');//change this to only favorited game array
+
+/*
+ * GET favorited games page.
+ */
+exports.favoritesview = function(req, res){
+  var favoritedGame;
+  for(i=0;i<gamedata.games.length; i++){
+    if( gamedata.games[i].isFavorited == true){
+      favoritedGame.push(gamedata.games[i]);
+    }
+  }
+  console.log(favoritedGame);
+  res.render('savedgames', favoritedGame);
+};
+
 
 /*
  * GET saved games page.
  */
 exports.view = function(req, res){
-  console.log(gamedata);
   res.render('savedgames', gamedata);
 };
