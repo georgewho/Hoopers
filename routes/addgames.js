716996d818a8e716bf177cced7863e0d3febbf9b
George Hu <george98.hu@gmail.com>
2019-02-26 20:07:38 -0800

    first iteration of google analytics on home page

--- routes/addgames.js
+++ routes/addgames.js
@@ -1,36 +1,35 @@
 //Get our game data
 var gamedata = require('../game.json');
 
- /*
+/*
  * GET new stats
  */
 exports.addGamesview = function(req, res) {
 
-    //console.log(req.query.date);
-    //console.log(req.query.points);
+        //console.log(req.query.date);
+        //console.log(req.query.points);
 
-    newGame = {
-        "id": gamedata.games.length,
-        "mapID": "mapArea1",  //FIX
-        "name": req.query.name,
-        "time": req.query.time,
-        "date": req.query.date,
-        "skill": req.query.skill,
-        "players": req.query.players,
-        "distance": req.query.distance,
-        "isFavorited" : false
-    };
-    console.log(newGame);
-    gamedata.games.push(newGame);
-    
-    console.log(gamedata);
-    
-    res.render('savedgames',gamedata);
-}
-/*
- * GET home page.
- */
+        newGame = {
+            "id": gamedata.games.length,
+            "name": req.query.name,
+            "time": req.query.time,
+            "date": req.query.date,
+            "skill": req.query.skill,
+            "players": req.query.players,
+            "distance": req.query.distance,
+            "isFavorited": false
+        };
+        console.log(newGame);
+        gamedata.games.push(newGame);
+
+        console.log(gamedata);
+
+        res.render('savedgames', gamedata);
+    }
+    /*
+     * GET home page.
+     */
 
 exports.view = function(req, res) {
-    res.render('addgames',gamedata);
+    res.render('addgames', gamedata);
 };
