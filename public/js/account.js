96d8ad6b8c9faad19dc6940e39cfeb59ee6d148a
anuraj <anurajd@gmail.com>
2019-03-07 21:03:00 -0800

    Fixed profile page to save data

--- public/js/account.js
+++ public/js/account.js
@@ -1,5 +1,7 @@
 $(function() {
     initializePage();
+    saveSkill();
+    saveNotes();
 });
 function initializePage() {
         var user = localStorage.username;
@@ -19,3 +21,26 @@ function initializePage() {
         $('.userphoto').hide();
     }
 }
+
+function saveSkill() {
+    var skillValue = localStorage.getItem("skillValue");
+
+    if(skillValue != null) {
+        $("select[name=skill]").val(skillValue);
+    }
+        $("select[name=skill]").on("change", function() {
+            localStorage.setItem("skillValue", $(this).val());
+        });
+}
+
+function saveNotes() {
+    var noteValue = localStorage.getItem("noteValue");
+    
+    if(noteValue != null) {
+        $("input[name=notes]").val(noteValue);
+    }
+        $("input[name=notes]").on("change", function() {
+            localStorage.setItem("noteValue", $(this).val());
+    });
+}
+
